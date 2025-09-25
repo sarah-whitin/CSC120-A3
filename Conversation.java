c1368416b30138e59953465c144f46368bff2fb9
S. Whitin <se.whitin@gmail.com>
2025-09-24 21:45:39 -0400

    rounds + cannings

--- Conversation.java
+++ Conversation.java
@@ -1,21 +1,79 @@
+import java.util.Scanner;
+import java.lang.Math;
 
 class Conversation implements ConversationRequirements {
 
-  // Attributes 
+  // Attributes
+  String [] goodbyes;
+
+  String [] phrases;
 
   /**
    * Constructor 
    */
-  Conversation() {
+  Conversation(String [] g, String [] p) {
+    this.goodbyes = g;
+    this.phrases = p;
+  }
+
+  public String response(){
+    // defining the range 
+    int max = 5;
+    int min = 0;
+    int range = max - min + 1;
     
-  }
+    int rand = (int)(Math.random() * range) + min;
+    // getting that string from the array
 
-  /**
-   * Starts and runs the conversation with the user
-   */
+    String respo = this.phrases[rand];
+    return respo;
+  };
+
+  public String bye(){// defining the range 
+    int max = 3;
+    int min = 0;
+    int range = max - min + 1;
+    
+    int rand = (int)(Math.random() * range) + min;
+    // getting that string from the array
+
+    String respo = this.goodbyes[rand];
+    return respo;};
+
   public void chat() {
+    Scanner input = new Scanner(System.in);
+
+    // ask for how many chats you want
+    System.out.println("How many rounds of chatting do you want?");
+    int rounds = input.nextInt();
+
+    // what to do if someone types in not a number?
+
+    int counter = 0;
+    // while True
+    while (true){
+      // get an input
+      String userInput = input.nextLine();
+
+      // give a print statement 
+      String respo = this.response();
+      System.out.println(respo);
+
+      // add to counter
+      counter = counter+1;
+      if (counter == rounds+1){
+        String bye = this.bye();
+        System.out.println(bye);
+        break;
+      }
+      // when counter == rounds
+        // break
+    }
+    
 
   }
+    // gets user input
+    // responds that many time
 
   /**
    * Prints transcript of conversation
@@ -35,10 +93,13 @@ class Conversation implements ConversationRequirements {
   }
 
   public static void main(String[] arguments) {
+    String [] goodbyes = {"See ya", "Bye-bye", "Bye now", "Catch ya later!"};
 
-    Conversation myConversation = new Conversation();
+    String [] phrases = {"Whoa", "Yeah ...", "Hm-mm", "I see", "Understood", "Heh"};
+
+    Conversation myConversation = new Conversation(goodbyes,phrases);
     myConversation.chat();
-    myConversation.printTranscript();
+  //  myConversation.printTranscript();
 
   }
 }
--- reflection.md
+++ reflection.md
@@ -1,6 +1,7 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+    Jessie W
 - Which session(s) of TA / office hours did you attend?
 - What are your initial impressions of Java? 
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
